3a34331ac25064b3395f56a8c2e7303f130e6ebe
Shehroz Raxmatov <98930700+Shehroz8@users.noreply.github.com>
2022-09-25 21:22:42 +0500

    Update usePrevious.js

--- src/hook/usePrevious.js
+++ src/hook/usePrevious.js
@@ -16,5 +16,12 @@ function usePrevious(value) {
   return ref.current;
 }
 
+function usePrevious(value) {
+  const ref = useRef();
+  useRffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+}
 export { usePrevious };
 
